2365680ef5d88273371e1ca847c83dfcc6d0fde7
Paul Varner <fuzzyray@gmail.com>
2021-02-22 20:46:23 -0600

    Implement mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,28 @@
 import numpy as np
 
-def calculate(list):
 
+def calculate(data_list):
+    if len(data_list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    matrix = np.array(data_list).reshape((3, 3))
 
+    calculations = {
+        "mean": [list(np.mean(matrix, axis=0)), list(np.mean(matrix, axis=1)), float(np.mean(matrix))],
+        "variance": [list(np.var(matrix, axis=0)), list(np.var(matrix, axis=1)), float(np.var(matrix))],
+        "standard deviation": [list(np.std(matrix, axis=0)), list(np.std(matrix, axis=1)), float(np.std(matrix))],
+        "max": [list(np.max(matrix, axis=0)), list(np.max(matrix, axis=1)), float(np.max(matrix))],
+        "min": [list(np.min(matrix, axis=0)), list(np.min(matrix, axis=1)), float(np.min(matrix))],
+        "sum": [list(np.sum(matrix, axis=0)), list(np.sum(matrix, axis=1)), float(np.sum(matrix))],
+    }
+
+    # calculations = {
+    #     'mean': [axis1, axis2, flattened],
+    #     'variance': [axis1, axis2, flattened],
+    #     'standard deviation': [axis1, axis2, flattened],
+    #     'max': [axis1, axis2, flattened],
+    #     'min': [axis1, axis2, flattened],
+    #     'sum': [axis1, axis2, flattened]
+    # }
 
     return calculations
